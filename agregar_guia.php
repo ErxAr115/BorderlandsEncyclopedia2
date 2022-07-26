a2fba6525bb5235c8b0be7fd2827a6ff6d072591
ErxAr115 <ernesto_armenta_2000@hotmail.com>
2022-07-25 18:40:26 -0600

    Se termino la opcion de agregar guia

---
+++ agregar_guia.php
@@ -0,0 +1,30 @@
+<?php
+    include("conexion.php");
+    $link = Conectar();
+
+    if($_POST){
+        $InsertQuery = "INSERT INTO $tablaG (titulo,contenido,imagen,video) VALUES ('".$_POST['titulo']."','".$_POST['contenido']."','".$_POST['imagen']."','".$_POST['video']."')";
+
+        $result = mysqli_query($link, $InsertQuery);
+
+        if($result) {
+            echo "<script type='text/javascript'>alert('Los datos se insertaron correctamente.');</script>";
+        } else {
+            echo "<script type='text/javascript'>alert('No se insertaron los datos.');</script>";
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/Logo_B.png">
+    <title>Borderlands Encyclopedia</title>
+</head>
+<body>
+    <a href="administracion.html">Volver al manejador</a>
+</body>
+</html>
